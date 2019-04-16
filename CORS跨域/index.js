2006203ce0dc95d9fac8683bc83e8c1eacf9c451
saber2pr <saber2pr@gmail.com>
2019-04-16 18:09:16 +0800

    update

--- CORS跨域/index.js
+++ CORS跨域/index.js
@@ -1,14 +1,16 @@
-/*
- * @Author: saber2pr
- * @Date: 2019-04-16 17:38:53
- * @Last Modified by: saber2pr
- * @Last Modified time: 2019-04-16 18:03:01
- */
-const request = axios.create({
-  baseURL: 'http://localhost:3005',
-  timeout: 1000
-})
-// 当前域为http://localhost:3005/时，不跨域
-// 当前域为http://localhost:3006/，跨域（端口不一致）
-// 与http://localhost:3005的后端约定，后端设置cors响应头为当前域
-request.get('http://localhost:3005/cors_test').then(console.log)
+function main() {
+  /*
+   * @Author: saber2pr
+   * @Date: 2019-04-16 17:38:53
+   * @Last Modified by: saber2pr
+   * @Last Modified time: 2019-04-16 18:05:11
+   */
+  const request = axios.create({
+    baseURL: 'http://localhost:3005',
+    timeout: 1000
+  })
+  // 当前域为http://localhost:3005/时，不跨域
+  // 当前域为http://localhost:3006/，跨域（端口不一致）
+  // 与http://localhost:3005的后端约定，后端设置cors响应头为当前域
+  request.get('http://localhost:3005/cors_test').then(console.log)
+}
--- JSONP跨域/index.js
+++ JSONP跨域/index.js
@@ -1,35 +1,37 @@
-/*
- * @Author: saber2pr
- * @Date: 2019-04-16 16:42:18
- * @Last Modified by: saber2pr
- * @Last Modified time: 2019-04-16 17:15:03
- */
-/**
- * @param {string} url
- */
-const jsonp = url =>
-  new Promise((resolve, reject) => {
-    // 创建标签
-    const script = document.createElement('script')
-    // 设置回调名
-    const callbackId = `jsonp_${Date.now()}`
-    // 参数
-    const params = url.split('?')[1] || ''
-    // 拼接请求的url，异步请求在这里
-    script.src = url.includes('?')
-      ? `${url}&callback=${callbackId}`
-      : `${url}?callback=${callbackId}&${params}`
-    // 设置读取返回结果的回调函数, 必须设置在window上
-    window[callbackId] = result => {
-      // 释放内存
-      delete window[callbackId]
-      document.body.removeChild(script)
-      // 结果
-      result ? resolve(result) : reject('404')
-    }
-    script.addEventListener('error', () => reject('script create fail'))
-    // 发出请求
-    document.body.appendChild(script)
-  })
+function main() {
+  /*
+   * @Author: saber2pr
+   * @Date: 2019-04-16 16:42:18
+   * @Last Modified by: saber2pr
+   * @Last Modified time: 2019-04-16 18:04:43
+   */
+  /**
+   * @param {string} url
+   */
+  const jsonp = url =>
+    new Promise((resolve, reject) => {
+      // 创建标签
+      const script = document.createElement('script')
+      // 设置回调名
+      const callbackId = `jsonp_${Date.now()}`
+      // 参数
+      const params = url.split('?')[1] || ''
+      // 拼接请求的url，异步请求在这里
+      script.src = url.includes('?')
+        ? `${url}&callback=${callbackId}`
+        : `${url}?callback=${callbackId}&${params}`
+      // 设置读取返回结果的回调函数, 必须设置在window上
+      window[callbackId] = result => {
+        // 释放内存
+        delete window[callbackId]
+        document.body.removeChild(script)
+        // 结果
+        result ? resolve(result) : reject('404')
+      }
+      script.addEventListener('error', () => reject('script create fail'))
+      // 发出请求
+      document.body.appendChild(script)
+    })
 
-jsonp('http://localhost:3005/jsonp?name=saber2pr&age=21').then(console.log)
+  jsonp('http://localhost:3005/jsonp?name=saber2pr&age=21').then(console.log)
+}
