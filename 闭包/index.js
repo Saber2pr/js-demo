6ebefbd687428d5883b881d3a629d9901e74a9f5
saber2pr <saber2pr@gmail.com>
2019-04-16 12:39:44 +0000

    update

--- 闭包/index.js
+++ 闭包/index.js
@@ -1,76 +1,78 @@
-/*
- * @Author: saber2pr 
- * @Date: 2019-04-14 11:16:39 
- * @Last Modified by: saber2pr
- * @Last Modified time: 2019-04-16 10:59:28
- */
 function main() {
-  if (1) {
-    // var变量声明预编译提升到上一层，或者说if没有块级
-    var val = 233
-  }
+  /*
+   * @Author: saber2pr 
+   * @Date: 2019-04-14 11:16:39 
+   * @Last Modified by: saber2pr
+   * @Last Modified time: 2019-04-16 12:37:01
+   */
+  function main() {
+    if (1) {
+      // var变量声明预编译提升到上一层，或者说if没有块级
+      var val = 233
+    }
 
-  try {
-    console.log(val) // 233
-  } catch (error) {
-    console.error(error)
-  }
-
-  if (1) {
     try {
-      console.log(_val) // 没有初始化，即暂定死区。改成var声明会输出undefined，var默认初始化为undefined
+      console.log(val) // 233
     } catch (error) {
-      console.log(error)
-    }
-    // let块作用域
-    let _val = 344
-  }
-
-  try {
-    console.log(_val) // _val is not defined，因为_val在块作用域内(子作用域)
-  } catch (error) {
-    console.error(error)
-  }
-
-  // 函数作用域也是块级
-  function Block() {
-    // 函数封闭作用域
-    var _val_0 = 344
-  }
-
-  try {
-    console.log(_val_0) // _val_0 is not defined
-  } catch (error) {
-    console.error(error)
-  }
-
-  const People = (function () {
-    // iife闭包环境
-
-    // 被闭包的变量，所有实例共享
-    var _age = 21
-
-    function People(name) {
-      this.name = name
+      console.error(error)
     }
 
-    People.prototype.getAge = function () {
-      return _age
+    if (1) {
+      try {
+        console.log(_val) // 没有初始化，即暂定死区。改成var声明会输出undefined，var默认初始化为undefined
+      } catch (error) {
+        console.log(error)
+      }
+      // let块作用域
+      let _val = 344
     }
 
-    People.prototype.addAge = function () {
-      _age++
+    try {
+      console.log(_val) // _val is not defined，因为_val在块作用域内(子作用域)
+    } catch (error) {
+      console.error(error)
     }
 
-    return People
-  })()
+    // 函数作用域也是块级
+    function Block() {
+      // 函数封闭作用域
+      var _val_0 = 344
+    }
 
-  const me = new People('saber2pr')
-  me.addAge()
-  console.log(me.getAge()) // 22
+    try {
+      console.log(_val_0) // _val_0 is not defined
+    } catch (error) {
+      console.error(error)
+    }
 
-  const saber = new People('saber')
-  saber.addAge()
-  console.log(saber.getAge()) // 23
+    const People = (function () {
+      // iife闭包环境
 
+      // 被闭包的变量，所有实例共享
+      var _age = 21
+
+      function People(name) {
+        this.name = name
+      }
+
+      People.prototype.getAge = function () {
+        return _age
+      }
+
+      People.prototype.addAge = function () {
+        _age++
+      }
+
+      return People
+    })()
+
+    const me = new People('saber2pr')
+    me.addAge()
+    console.log(me.getAge()) // 22
+
+    const saber = new People('saber')
+    saber.addAge()
+    console.log(saber.getAge()) // 23
+
+  }
 }
